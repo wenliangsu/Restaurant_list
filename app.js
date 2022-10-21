35b88030d054b8c3deff7fe3f983fbae1fd0e9c2
wenliangsu <wenliangsu65018@gmail.com>
2022-10-21 13:03:19 +0800

    Fix font-awesome link and style

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
 //Section Set the variable
-const express = require('express')
-const app = express()
-const exphbs = require('express-handlebars')
-
+const express = require("express");
+const app = express();
+const exphbs = require("express-handlebars");
 
 //server related variable
-const port = 3000
+const port = 3000;
 
 //Section Set template engine
-app.engine('handlebars', exphbs({defaultLayout: 'main'}))
-app.set('view engine', 'handlebars')
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 //Section Static file
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-
-//Section Routes setting 
+//Section Routes setting
 //todo 待修正
-app.get('/', (req, res) => {
-  res.render('index')
-})
-
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 //Section Express server start and listen
 app.listen(port, () => {
-  console.log(`Express is listening on localhost:${port}`)
-})
+  console.log(`Express is listening on localhost:${port}`);
+});
