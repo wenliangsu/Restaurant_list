c7e01134ed498d084fb117a06da8f28efb316646
wenliangsu <wenliangsu65018@gmail.com>
2022-10-21 22:23:45 +0800

    Add the description of restaurant

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const exphbs = require("express-handlebars");
 
 //invoke the list of data
-const restaurantList = require('./restaurant.json')
+const restaurantList = require("./restaurant.json");
 
 //server connection port
 const port = 3000;
@@ -20,9 +20,22 @@ app.use(express.static("public"));
 //Section Routes setting
 //todo set the route to list
 app.get("/", (req, res) => {
-  
   res.render("index", { restaurantList: restaurantList.results });
 });
+
+//todo set the route to the description of restaurant(params)
+app.get("/restaurants/:restaurant_id", (req, res) => {
+  // console.log(req.params.restaurant_id);
+  const restaurantId = restaurantList.results.find(
+    (restaurant) => restaurant.id.toString() === req.params.restaurant_id
+  );
+
+  // console.log(restaurantId);
+
+  res.render("show", { restaurantInfo: restaurantId });
+});
+
+//todo set the route for search bar
 
 //Section Express server start and listen
 app.listen(port, () => {
